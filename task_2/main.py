785b33543e8bd0ec56812709d88610f8b124a305
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-12-10 04:17:02 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -15,27 +15,28 @@ qualification_input = input("Введите номер квалификации:
 found_list = []# Создаём пустой список
 
 for item in data:# Создаём цикл, который будет пробегаться по переменной data, которую мы создали раньше
-    fields = item.get("fields", {})
-    item_code = fields.get("code")
+    fields = item.get("fields", {})# Извлекает значение по ключу "fields" из текущего item, или пустой словарь, если ключ отсутствует
+    item_code = fields.get("code")# Извлекает значение по ключу "code" из текущего fields
 
-    if  item_code == qualification_input:
-        model = item.get("model")
-        title = fields.get("title")
-        output_line = ""
+    if  item_code == qualification_input:# Условная проверка: выполняет следующий блок если извлеченный item_code совпадает с искомым qualification_input
+        model = item.get("model")# Извлекает значение по ключу "model" из текущего item
+        title = fields.get("title")# Извлекает значение по ключу "title" из текущего fields
+        output_line = ""# Инициализирует переменную как пустую строку
 
-        if model == "data.specialty":
-            c_type = fields.get("c_type", "")
-            output_line = f"{item_code} >> Специальность \"{title}\", {c_type}"
-        elif model == "data.skill":
-            output_line = f"{item_code} >> Квалификация \"{title}\""
+        if model == "data.specialty":# Проверяем равны ли значения переменной model и "data.specialty"
+            c_type = fields.get("c_type", "")# Извлекает значение по ключу "fields" из текущего item, или пустой словарь, если ключ отсутствует
+            output_line = f"{item_code} >> Специальность \"{title}\", {c_type}"# Выводим форматированную строку
+        elif model == "data.skill":# Проверяем равны ли значения переменной model и "data.skill"
+            output_line = f"{item_code} >> Квалификация \"{title}\""# Выводим форматированную строку
 
-        if output_line:
-            found_list.append(output_line)
+        if output_line:# Проверяем: если строка output_line не пуста, то
+            found_list.append(output_line)# Добавляем её в созданный ранее список
 
-if found_list:
-    print("=============== Найдено ===============")
-    for i in found_list:
-        print(i)
-else:
-    print("=============== Не найдено ===============")
+if found_list:# Проверяем: если список found_list не пуста, то
+    print("=============== Найдено ===============")# Выводим эту строку
+    for i in found_list:# Создаём цикл, который будет пробегаться по этому списку и 
+        print(i)# Выводим элементы этого списка
+else:# Иначе
+    print("=============== Не найдено ===============")# Выводим эту строку
+
 
